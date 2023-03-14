9e29b53446258a4f34942fdff47e999a490c6bf8
Gabriel Schulte <gabrielmdu@gmail.com>
2023-03-14 22:23:41 +0200

    Added common functions file

---
+++ resources/js/common.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @param {*} links 
+ * @param {*} meta 
+ * @returns 
+ */
+export const getLinksFromLaravelPagination = (links, meta) => {
+    return {
+        firstLink: links.first,
+        prevLink: links.prev,
+        nextLink: links.next,
+        lastLink: links.last,
+        lastPage: meta.last_page,
+        currentPage: meta.current_page,
+        allLinks: meta.links,
+    };
+};
+
+/**
+ * Remove empty properties from an object.
+ * Example: the object `{ prop1: 1, prop2: null } turns to { prop1: 1 }`
+ * 
+ * @param {object} obj The object to be cleaned
+ * @returns 
+ */
+export const getCleanObject = obj => Object.fromEntries(Object.entries(obj).filter(([_, v]) => v != null && v !== ''));
