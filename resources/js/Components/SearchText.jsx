9b27f7b613fc93d3bd844da1d678c693fbfe44e8
Gabriel Schulte <gabrielmdu@gmail.com>
2023-03-15 14:13:27 +0200

    Created a text search component

---
+++ resources/js/Components/SearchText.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { useUpdateEffect } from "react-use";
+
+/**
+ * Component for text searching within a given time
+ * 
+ * @param {object} props
+ * @param {(e:Event) => void} props.searchCallback - Callback that will be executed
+ * @param {number} props.callbackTriggerTime - The timeout to execute the callback
+ */
+export default function SearchText({ text = '', searchCallback, callbackTriggerTime = 1000 }) {
+    const [searchText, setSearchText] = useState(text);
+    const [callbackTimeout, setCallbackTimeout] = useState(0);
+
+    useUpdateEffect(() => {
+        clearTimeout(callbackTimeout);
+
+        if (searchText.length === 0) {
+            searchCallback(searchText);
+        } else {
+            setCallbackTimeout(setTimeout(() => searchCallback(searchText), callbackTriggerTime));
+        }
+    }, [searchText]);
+
+    return (
+        <div className='relative flex items-center mr-4 w-full max-w-sm'>
+            <div className='flex w-full bg-white rounded shadow'>
+                <input
+                    className='relative px-6 py-2 w-full rounded focus:shadow-outline'
+                    type="text"
+                    placeholder="Search…"
+                    autoComplete="false"
+                    spellCheck="false"
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                />
+            </div>
+            <button
+                className='absolute right-4 font-bold text-gray-400'
+                onClick={() => setSearchText('')}
+            >
+                x
+            </button>
+        </div>
+    );
+}
