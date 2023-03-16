0e47a9b99f5bdd08b877a4a05749cba963841ec9
Gabriel Schulte <gabrielmdu@gmail.com>
2023-03-16 12:01:33 +0200

    Added API route test for album search by text

--- tests/Feature/API/AlbumsRouteTest.php
+++ tests/Feature/API/AlbumsRouteTest.php
@@ -40,4 +40,25 @@ class AlbumsRouteTest extends TestCase
         $response->assertOk();
         $response->assertJsonCount(15, 'data');
     }
+
+    public function test_can_filter_albums_by_text()
+    {
+        $this->user->albums()->saveMany(
+            Album::factory()
+                ->count(20)
+                ->sequence(
+                    ['name' => 'ABC', 'description' => 'GHI'],
+                    ['name' => 'DEF', 'description' => 'JKL'],
+                )
+                ->create(['user_id' => $this->user->id])
+        );
+
+        Sanctum::actingAs($this->user);
+
+        $response = $this->get(route('api.albums.index', ['search' => 'abc']));
+        $response->assertJsonCount(10, 'data');
+
+        $response = $this->get(route('api.albums.index', ['search' => 'bcde']));
+        $response->assertJsonCount(0, 'data');
+    }
 }
