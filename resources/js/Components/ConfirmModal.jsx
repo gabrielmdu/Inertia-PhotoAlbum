baffe781c1ed204ac9fecd3701aa0c737fd4d2b3
Gabriel Schulte <gabrielmdu@gmail.com>
2023-03-23 10:45:37 +0200

    Little adjustments to confirm modal + button component

--- resources/js/Components/ConfirmModal.jsx
+++ resources/js/Components/ConfirmModal.jsx
@@ -1,5 +1,7 @@
+import { BUTTON_TYPE } from "@/common";
 import { IconAlertCircle, IconQuestionMark } from "@tabler/icons-react";
 import React from "react";
+import Button from "./Button/Button";
 import Modal from "./Modal";
 
 const ConfirmModal = ({ onConfirm, onCancel, className = '', children, ...props }) => {
@@ -7,25 +9,23 @@ const ConfirmModal = ({ onConfirm, onCancel, className = '', children, ...props 
         <Modal {...props}>
             <div className={'px-5 py-3 bg-gradient-to-b from-gray-100 to-gray-300 ' + className}>
                 <div className="text-sky-600"><IconAlertCircle size={24} /></div>
-                <div className="my-4">
+                <div className="my-6">
                     {children}
                 </div>
 
-                <div className="flex items-center justify-end py-4 border-t-2 border-gray-300">
-                    <button
-                        type="button"
-                        className="rounded px-5 py-2 mr-4 bg-green-600 text-gray-100 text-sm"
+                <div className="flex items-center gap-4 justify-end pt-4 pb-2 border-t-2 border-gray-400 border-opacity-50">
+                    <Button
+                        colorType={BUTTON_TYPE.SUCCESS}
                         onClick={onConfirm}
                     >
                         Confirm
-                    </button>
-                    <button
-                        type="button"
-                        className="rounded px-5 py-2 bg-red-600 text-gray-100 text-sm"
+                    </Button>
+                    <Button
+                        colorType={BUTTON_TYPE.ERROR}
                         onClick={onCancel}
                     >
                         Cancel
-                    </button>
+                    </Button>
                 </div>
             </div>
         </Modal>
