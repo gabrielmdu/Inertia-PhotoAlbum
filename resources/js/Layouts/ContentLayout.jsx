79e5e1935ff8ec259a9ba975ed2e472b57caec64
Gabriel Schulte <gabrielmdu@gmail.com>
2023-03-26 13:01:17 +0300

    Content layout with possible classes addition

--- resources/js/Layouts/ContentLayout.jsx
+++ resources/js/Layouts/ContentLayout.jsx
@@ -2,7 +2,7 @@ import { Head, usePage } from "@inertiajs/react";
 import React from "react";
 import AuthenticatedLayout from "./AuthenticatedLayout";
 
-export default function Content({ title, children }) {
+export default function Content({ title, className = '', children }) {
     const { errors } = usePage().props;
 
     return (
@@ -11,7 +11,7 @@ export default function Content({ title, children }) {
 
             <div className="flex flex-1 flex-col justify-center items-center py-3 sm:py-6">
                 <div className="flex flex-1 w-full max-w-7xl h-full sm:px-6 lg:px-8">
-                    <div className="flex-1 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div className={"flex-1 bg-white overflow-hidden shadow-sm sm:rounded-lg " + className}>
                         <div className="relative min-h-full p-6 text-gray-900">
                             {children}
                         </div>
