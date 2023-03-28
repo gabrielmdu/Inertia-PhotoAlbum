9a2c7d3436e88df74da2bce3cae2b34ad0162309
Gabriel Schulte <gabrielmdu@gmail.com>
2023-03-28 12:20:12 +0300

    Album show layout responsive

--- resources/js/Pages/Albums/Show.jsx
+++ resources/js/Pages/Albums/Show.jsx
@@ -9,20 +9,22 @@ const ShowAlbum = ({ album }) => {
             title={'Album ' + album.data.name}
             className='bg-gradient-to-b from-violet-900 via-purple-900 to-zinc-900'
         >
-            <div className="flex border-b border-gray-500 pb-4">
+            <div className="flex flex-col sm:flex-row border-b border-gray-500 pb-4">
                 <div className="absolute right-6 top-6 text-white">
                     <Link href={route('albums.edit', { album: album.data.id })} title="Edit album">
                         <IconEdit size={28} stroke={1} />
                     </Link>
                 </div>
 
-                <div className="rounded-full overflow-hidden border-4 border-zinc-200 w-72">
-                    <img src={`https://picsum.photos/id/${album.data.cover_id}/300`} alt="Album cover" />
+                <div className="flex justify-center w-full sm:w-72 h-60 sm:h-72">
+                    <div className="rounded-full overflow-hidden border-4 border-zinc-200 w-60 sm:w-full h-full">
+                        <img src={`https://picsum.photos/id/${album.data.cover_id}/300`} alt="Album cover" />
+                    </div>
                 </div>
 
                 <div className="flex flex-1 flex-col justify-center gap-4 px-4 py-3">
-                    <div className="text-5xl font-mono text-lime-400">{album.data.name}</div>
-                    <div className="text-xl italic text-gray-100">{album.data.description}</div>
+                    <div className="text-3xl sm:text-5xl font-mono text-lime-400">{album.data.name}</div>
+                    <div className="text-md sm:text-xl italic text-gray-100">{album.data.description}</div>
                 </div>
             </div>
         </ContentLayout>
