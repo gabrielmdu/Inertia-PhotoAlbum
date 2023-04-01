76dda2f938027330ec734e585a0bd485dd18073f
Gabriel Schulte <gabrielmdu@gmail.com>
2023-04-01 15:21:31 +0300

    paginated results hook preventing initial loading

--- resources/js/Hooks/usePaginatedResults.js
+++ resources/js/Hooks/usePaginatedResults.js
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 
 export const usePaginatedResults = (url, onResult) => {
-    const [currPage, setCurrPage] = useState(1);
+    const [currPage, setCurrPage] = useState(0);
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         async function getPaginatedResults(url) {
             try {
+                if (currPage === 0) {
+                    return;
+                }
+
                 setIsLoading(true);
 
                 const response = await axios.get(url, {
