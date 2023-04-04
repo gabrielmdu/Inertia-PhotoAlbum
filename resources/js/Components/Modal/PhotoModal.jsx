aea671770c153f4119272d3bf0a508ba6b9d7196
Gabriel Schulte <gabrielmdu@gmail.com>
2023-04-04 16:49:18 +0300

    Photo modal with div pre-sized + loading state

--- resources/js/Components/Modal/PhotoModal.jsx
+++ resources/js/Components/Modal/PhotoModal.jsx
@@ -1,13 +1,15 @@
 import { getPicsumPhoto } from "@/common";
-import React from "react";
+import React, { useState } from "react";
 import Modal from "./Modal";
 
 const PhotoModal = ({ photo, ...props }) => {
+    const [isLoading, setIsLoading] = useState(true);
+
     return (
         <Modal maxWidth="xl" {...props}>
             <div className="flex flex-col items-center">
-                <div className="w-full">
-                    <img src={getPicsumPhoto(photo.api_id, 600, 700)} alt="Photo" />
+                <div className={"relative w-full h-0 pb-[114.3%] bg-gradient-to-b from-gray-200 to-gray-400 " + (isLoading ? 'animate-pulse' : '')}>
+                    <img className="absolute block w-full h-full" onLoad={() => setIsLoading(false)} src={getPicsumPhoto(photo.api_id, 600, 700)} alt="Photo" />
                 </div>
                 <div className="p-2">
                     {photo.caption}
