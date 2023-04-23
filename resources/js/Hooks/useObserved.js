6b4f001d81fd45c247bf6577f9f8d1b2dc4e5ca5
Gabriel Schulte <gabrielmdu@gmail.com>
2023-04-24 01:52:48 +0300

    Observed hook with timer threshold

--- resources/js/Hooks/useObserved.js
+++ resources/js/Hooks/useObserved.js
@@ -1,15 +1,34 @@
 import { useCallback, useEffect, useState, useRef } from 'react';
 
-export const useObserved = (refObserved) => {
+export const useObserved = (refObserved, threshold = 500) => {
     const refObserver = useRef();
+    const canObserve = useRef(true);
+    const observeWhenAvailable = useRef(false);
 
     const [isObserved, setIsObserved] = useState(false);
 
-    const startObserving = () => refObserver.current.observe(refObserved.current);
+    const startObserving = () => {
+        if (canObserve.current) {
+            refObserver.current.observe(refObserved.current);
+            canObserve.current = false;
+
+            setTimeout(() => {
+                canObserve.current = true;
+
+                if (observeWhenAvailable.current) {
+                    observeWhenAvailable.current = false;
+                    startObserving();
+                }
+            }, threshold);
+        } else {
+            observeWhenAvailable.current = true;
+        }
+    };
+
     const stopObserving = () => {
         refObserver.current.unobserve(refObserved.current);
         setIsObserved(false);
-    }
+    };
 
     useEffect(() => {
         refObserver.current = new IntersectionObserver(handleObserved);
