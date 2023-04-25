b0e9171e13edce0cb614657e581c347c65b9ecf0
Gabriel Schulte <gabrielmdu@gmail.com>
2023-04-26 02:46:24 +0300

    Adjusted album slider style + time ago

--- resources/js/Components/Album/AlbumSlider.jsx
+++ resources/js/Components/Album/AlbumSlider.jsx
@@ -1,5 +1,7 @@
 import Album from './Album';
 import Glider from 'react-glider';
+import ReactTimeAgo from 'react-time-ago';
+import { IconCircleArrowLeft, IconCircleArrowRight } from '@tabler/icons-react';
 
 import "glider-js/glider.min.css";
 import "react-glider/glider.defaults.css";
@@ -14,7 +16,7 @@ const AlbumSlider = ({ albums }) => {
             },
         },
         {
-            breakpoint: 880,
+            breakpoint: 920,
             settings: {
                 slidesToShow: 3,
             },
@@ -25,14 +27,20 @@ const AlbumSlider = ({ albums }) => {
         <Glider
             draggable
             hasArrows
+            scrollLock
+            iconLeft={<IconCircleArrowLeft size={28} />}
+            iconRight={<IconCircleArrowRight size={28} />}
             responsive={options}
             slidesToShow={1}
             slidesToScroll={1}
         >
             {
                 albums.map(album =>
-                    <div key={album.id} className='bg-transparent'>
-                        <Album album={album} />
+                    <div key={album.id} className='flex flex-col'>
+                        <Album album={album} sizeClasses='w-5/6 sm:w-56 h-56 lg:w-64 lg:h-64 xl:w-72' />
+                        <div className='text-sm text-sky-950'>
+                            <ReactTimeAgo date={new Date(album.updated_at)} />
+                        </div>
                     </div>)
             }
         </Glider>
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -12,14 +12,16 @@ export default function Dashboard({ albums, photos }) {
         <ContentLayout title="Dashboard">
             <ContentHeader>Dashboard - {user.name}</ContentHeader>
 
-            <div className='mt-8 p-3 bg-gray-100 border border-purple-700 rounded'>
-                <div className='mb-2'>Your latest album activity</div>
+            <div className='mt-8 p-3 bg-gradient-to-t from-neutral-100 to-slate-200 border border-purple-700 rounded'>
+                <div className='p-2 text-gray-200 bg-sky-600 rounded-t'>Your latest album activity</div>
 
-                <div className='px-8'>
+                <div className='px-8 bg-gradient-to-r from-gray-100 to-neutral-200'>
                     <AlbumSlider albums={albums} />
                 </div>
 
-                <Link href={route('albums.index')} className='underline'>View all</Link>
+                <Link href={route('albums.index')} className='py-2 px-4 rounded bg-orange-400 text-white'>
+                    View all
+                </Link>
             </div>
 
         </ContentLayout>
