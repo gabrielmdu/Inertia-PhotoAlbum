833cf61489cb13e0aea291bcc47b934e04a0ca34
Gabriel Schulte <gabrielmdu@gmail.com>
2023-04-27 01:03:48 +0300

    Adjusted dashboard panel style

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -3,8 +3,6 @@ import ContentLayout from '@/Layouts/ContentLayout';
 import AlbumSlider from '@/Components/Album/AlbumSlider';
 import { Link, usePage } from '@inertiajs/react';
 
-
-
 export default function Dashboard({ albums, photos }) {
     const { auth: { user } } = usePage().props;
 
@@ -12,18 +10,19 @@ export default function Dashboard({ albums, photos }) {
         <ContentLayout title="Dashboard">
             <ContentHeader>Dashboard - {user.name}</ContentHeader>
 
-            <div className='mt-8 p-3 bg-gradient-to-t from-neutral-100 to-slate-200 border border-purple-700 rounded'>
-                <div className='p-2 text-gray-200 bg-sky-600 rounded-t'>Your latest album activity</div>
+            <div className='mt-8 bg-slate-200 rounded drop-shadow-lg'>
+                <div className='p-2 text-gray-200 bg-violet-800 rounded-t'>Your latest album activity</div>
 
-                <div className='px-8 bg-gradient-to-r from-gray-100 to-neutral-200'>
+                <div className='px-8'>
                     <AlbumSlider albums={albums} />
                 </div>
 
-                <Link href={route('albums.index')} className='py-2 px-4 rounded bg-orange-400 text-white'>
-                    View all
-                </Link>
+                <div className='pb-4 pl-4'>
+                    <Link href={route('albums.index')} className='py-2 px-3 rounded bg-cyan-400 text-white'>
+                        View all
+                    </Link>
+                </div>
             </div>
-
         </ContentLayout>
     );
 }
