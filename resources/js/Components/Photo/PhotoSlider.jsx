6d3f7912eefe3d49cfff8e92a5ae2947289fd3c5
Gabriel Schulte <gabrielmdu@gmail.com>
2023-04-27 23:47:02 +0300

    Fixed photo size classes in slider

--- resources/js/Components/Photo/PhotoSlider.jsx
+++ resources/js/Components/Photo/PhotoSlider.jsx
@@ -8,7 +8,7 @@ const PhotoSlider = ({ photos }) => {
             {
                 photos.map(photo =>
                     <div key={photo.id} className='flex flex-col'>
-                        <Photo photo={photo} className='min-w-0 min-h-0 sm:w-56 h-56 lg:w-64 lg:h-64 xl:w-72' />
+                        <Photo photo={photo} className='!min-w-0 !min-h-0 sm:w-56 h-56 lg:w-64 lg:h-64 xl:w-72' />
                         <div className='text-sm text-sky-950'>
                             <ReactTimeAgo date={new Date(photo.created_at)} />
                         </div>
