3589a60d2fbcd703af0e4d258fb79d913a0b5c5e
Gabriel Schulte <gabrielmdu@gmail.com>
2023-05-02 16:57:57 +0300

    Slider adjustment of slides according to count

--- resources/js/Components/Slider/Slider.jsx
+++ resources/js/Components/Slider/Slider.jsx
@@ -1,4 +1,5 @@
 import Glider from 'react-glider';
+import { useEffect, useRef } from 'react';
 import { IconCircleArrowLeft, IconCircleArrowRight } from '@tabler/icons-react';
 
 import "glider-js/glider.min.css";
@@ -6,20 +7,29 @@ import "react-glider/glider.defaults.css";
 import './Slider.scss';
 
 const Slider = ({ children }) => {
-    const options = [
-        {
-            breakpoint: 640,
-            settings: {
-                slidesToShow: 2,
+    const gliderRef = useRef(null);
+
+    // adjust the number of slides according to how many were added
+    useEffect(() => {
+        const slidesCount = gliderRef.current.slides.length;
+
+        const options = [
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: Math.min(slidesCount, 2),
+                },
             },
-        },
-        {
-            breakpoint: 920,
-            settings: {
-                slidesToShow: 3,
+            {
+                breakpoint: 920,
+                settings: {
+                    slidesToShow: Math.min(slidesCount, 3),
+                },
             },
-        },
-    ];
+        ];
+
+        gliderRef.current.setOption({ responsive: options });
+    }, [gliderRef]);
 
     return (
         <Glider
@@ -28,9 +38,9 @@ const Slider = ({ children }) => {
             scrollLock
             iconLeft={<IconCircleArrowLeft size={28} />}
             iconRight={<IconCircleArrowRight size={28} />}
-            responsive={options}
             slidesToShow={1}
             slidesToScroll={1}
+            ref={gliderRef}
         >
             {children}
         </Glider>
